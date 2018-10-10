7dd9dbf5880fcc0256fa379d449e8fd219ed8937
Oleg <gvozdyk23@gmail.com>
2018-10-10 18:58:08 +0300

    Slider

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 var burger = document.getElementById('burger');
 var menu = document.getElementById('burger-menu');
+var btnLeft = document.getElementById('left');
+var left = 0;
 
 burger.addEventListener('click', function() {
    menu.classList.toggle('menu_active');
 });
+
+btnLeft.onclick = function() {
+   var polosa = document.getElementById('poloska');
+    left = left - 1500;
+    if(left < -12000) {
+        left = 0;
+    }
+   polosa.style.left = left + 'px';
+};
